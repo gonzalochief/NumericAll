4e6659a7f8279a5800ef075106a14ba7baaa4d75
andikemgitopsbot <gonzalochief@gmail.com>
2023-12-24 08:37:01 -0500

    add matrix sum function

--- matrix/algebra.go
+++ matrix/algebra.go
@@ -1,3 +1,34 @@
+package matrix
+
+import (
+	"errors"
+
+	"golang.org/x/exp/constraints"
+)
+
+var ErrDivByZero = errors.New("divide by zero")
+var ErrMatSizeMissmatch = errors.New("matrix size missmatch")
+
+func MatrixAdd[Ord constraints.Ordered](a, b [][]Ord) (resVal [][]Ord, err error) {
+	sizeA := MatrixSize(a)
+	sizeB := MatrixSize(b)
+	if sizeA != sizeB {
+		return nil, ErrMatSizeMissmatch
+	}
+	rows := sizeA[0]
+	columns := sizeA[1]
+	// Expand output slice
+	for i := 0; i < rows; i++ {
+		resVal = append(resVal, make([]Ord, columns))
+	}
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			resVal[i][j] = a[i][j] + b[i][j]
+		}
+	}
+	return resVal, nil
+}
 
 // MatrixSize estimates the size of a 2D matrix
 // Input:
