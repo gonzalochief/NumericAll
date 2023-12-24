91bc35e5544a8755f7e32372f0e646a144ffc241
andikemgitopsbot <gonzalochief@gmail.com>
2023-12-24 11:36:48 -0500

    add test fail cases

--- nonlineareq/nonlinear_test.go
+++ nonlineareq/nonlinear_test.go
@@ -102,11 +102,12 @@ type testStructureBisect struct {
 	TestTol         float64
 	ExpectedValueC  float64
 	ExpectedValueYC float64
+	ExpectedErr     error
 }
 
 func TestBisect(t *testing.T) {
 	// Set test cases
-	testCases := make([]testStructureBisect, 3)
+	testCases := make([]testStructureBisect, 4)
 
 	testCases[0].TestFunction = func(x float64) float64 {
 		return x*math.Sin(x) - 1
@@ -136,12 +137,28 @@ func TestBisect(t *testing.T) {
 	testCases[2].TestTol = 0.01
 	testCases[2].ExpectedValueC = 3.14453125
 
+	testCases[3].TestFunction = func(x float64) float64 {
+		return 1 / (x - 2)
+	}
+	testCases[3].TestCaseName = "2.2.9.b fail"
+	testCases[3].TestA = 3
+	testCases[3].TestB = 7
+	testCases[3].TestTol = 0.01
+	testCases[3].ExpectedValueC = 1.9990234375
+	testCases[3].ExpectedErr = ErrFuncSignNotEqual
+
 	for _, tc := range testCases {
-		c, _, _, _ := BisectBolzano(tc.TestFunction, tc.TestA, tc.TestB, tc.TestTol)
-		if c != tc.ExpectedValueC {
-			t.Logf("yc: %f", tc.ExpectedValueYC)
-			t.Errorf("wrong estimation for case: %s. expecting: %f, receiving %f", tc.TestCaseName, tc.ExpectedValueC, c)
+		t.Logf("testing case number: %s", tc.TestCaseName)
+		c, _, _, err := BisectBolzano(tc.TestFunction, tc.TestA, tc.TestB, tc.TestTol)
+		if err == nil {
+			if c != tc.ExpectedValueC {
+				t.Logf("yc: %f", tc.ExpectedValueYC)
+				t.Errorf("wrong estimation for case: %s. expecting: %f, receiving %f", tc.TestCaseName, tc.ExpectedValueC, c)
+			}
+		} else if err != tc.ExpectedErr {
+			t.Error("expected error not catched") // Fail to catch error. Algorithm should not converge
 		}
+		t.Logf("testing case number: %s OK", tc.TestCaseName)
 	}
 }
 
@@ -154,10 +171,11 @@ type testStructRegulaFalsi struct {
 	TestEpsilon     float64
 	ExpectedValueC  float64
 	ExpectedValueYC float64
+	ExpectedErr     error
 }
 
 func TestRegulaFalsi(t *testing.T) {
-	testCases := make([]testStructRegulaFalsi, 1)
+	testCases := make([]testStructRegulaFalsi, 2)
 
 	testCases[0].TestFunction = func(x float64) float64 {
 		return x*math.Sin(x) - 1
@@ -170,14 +188,40 @@ func TestRegulaFalsi(t *testing.T) {
 	testCases[0].ExpectedValueC = 1.11415714303368
 	testCases[0].ExpectedValueYC = 0
 
+	testCases[1].TestFunction = func(x float64) float64 {
+		return x*math.Sin(x) - 1
+	}
+	testCases[1].TestCaseName = "ex. 2.8"
+	testCases[1].TestA = 0
+	testCases[1].TestB = 1
+	testCases[1].TestTol = 1e-12
+	testCases[1].TestEpsilon = 1e-12
+	testCases[1].ExpectedValueC = 1.11415714303368
+	testCases[1].ExpectedValueYC = 0
+	testCases[1].ExpectedErr = ErrFuncSignNotEqual
+
 	for _, tc := range testCases {
-		c, yC, absErr, _ := RegulaFalsi(tc.TestFunction, tc.TestA, tc.TestB, tc.TestTol, tc.TestEpsilon, 50)
-		if math.Abs(yC) > tc.TestEpsilon || absErr > tc.TestTol {
-			t.Errorf("wrong estimation for case: %s. tolerances exceeded", tc.TestCaseName)
+		t.Logf("testing case number: %s", tc.TestCaseName)
+		c, yC, absErr, err := RegulaFalsi(tc.TestFunction, tc.TestA, tc.TestB, tc.TestTol, tc.TestEpsilon, 50)
+		if err == nil {
+			if math.Abs(yC) > tc.TestEpsilon || absErr > tc.TestTol {
+				t.Errorf("wrong estimation for case: %s. tolerances exceeded", tc.TestCaseName)
+			}
+			if tc.ExpectedValueYC != 0 {
+				t.Errorf("wrong estimation for case: %s. expecting: %f, receiving %f", tc.TestCaseName, tc.ExpectedValueC, c)
+			}
+		} else if err != tc.ExpectedErr {
+			t.Error("expected error not catched") // Fail to catch error. Algorithm should not converge
 		}
-		if tc.ExpectedValueYC != 0 {
-			t.Errorf("wrong estimation for case: %s. expecting: %f, receiving %f", tc.TestCaseName, tc.ExpectedValueC, c)
+		t.Logf("testing case number: %s OK", tc.TestCaseName)
+		t.Logf("testing error signals case number: %s", tc.TestCaseName)
+		//test case: error maximum iterations reached
+		_, _, _, err = RegulaFalsi(tc.TestFunction, tc.TestA, tc.TestB, tc.TestTol, tc.TestEpsilon, 3)
+		switch err {
+		case nil:
+			t.Error("maximum iteration reached error not catched") // Fail to catch error. Algorithm should not converge
 		}
+		t.Logf("testing error signals case number: %s OK", tc.TestCaseName)
 	}
 }
 
@@ -231,6 +275,7 @@ func TestNewtonRaphson(t *testing.T) {
 	testCases[1].ExpectedIter = 18
 
 	for _, tc := range testCases {
+		t.Logf("testing case number: %s", tc.TestCaseName)
 		c, yC, _, i, err := NewtonRaphson(tc.TestY, tc.TestDY, tc.P0, tc.TestDelta, tc.TestEpsilon, tc.TestMaxIter)
 		if (math.Abs(tc.ExpectedValueP-c) > tc.TestDelta) && (math.Abs(tc.ExpectedValueYP) > tc.TestEpsilon) {
 			if math.Abs(tc.ExpectedValueP-c) > tc.TestDelta {
@@ -246,6 +291,15 @@ func TestNewtonRaphson(t *testing.T) {
 		if err != nil {
 			t.Errorf("maximum iteration reached: %d", i)
 		}
+		t.Logf("testing case number: %s OK", tc.TestCaseName)
+		t.Logf("testing error signals case number: %s", tc.TestCaseName)
+		//test case: error maximum iterations reached
+		_, _, _, _, err = NewtonRaphson(tc.TestY, tc.TestDY, tc.P0, tc.TestDelta, tc.TestEpsilon, 2)
+		switch err {
+		case nil:
+			t.Error("maximum iteration reached error not catched") // Fail to catch error. Algorithm should not converge
+		}
+		t.Logf("testing error signals case number: %s OK", tc.TestCaseName)
 	}
 }
 
@@ -293,6 +347,7 @@ func TestSecant(t *testing.T) {
 	testCases[1].ExpectedIter = 26
 
 	for _, tc := range testCases {
+		t.Logf("testing case number: %s", tc.TestCaseName)
 		c, yC, _, i, err := Secant(tc.TestY, tc.P0, tc.P1, tc.TestDelta, tc.TestEpsilon, tc.TestMaxIter)
 		if (math.Abs(tc.ExpectedValueP-c) > tc.TestDelta) && (math.Abs(tc.ExpectedValueYP) > tc.TestEpsilon) {
 			if math.Abs(tc.ExpectedValueP-c) > tc.TestDelta {
@@ -308,5 +363,15 @@ func TestSecant(t *testing.T) {
 		if err != nil {
 			t.Errorf("maximum iteration reached: %d", i)
 		}
+
+		t.Logf("testing case number: %s OK", tc.TestCaseName)
+		t.Logf("testing error signals case number: %s", tc.TestCaseName)
+		//test case: error maximum iterations reached
+		_, _, _, _, err = Secant(tc.TestY, tc.P0, tc.P1, tc.TestDelta, tc.TestEpsilon, 2)
+		switch err {
+		case nil:
+			t.Error("maximum iteration reached error not catched") // Fail to catch error. Algorithm should not converge
+		}
+		t.Logf("testing error signals case number: %s OK", tc.TestCaseName)
 	}
 }
