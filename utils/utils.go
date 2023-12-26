ba81e8dae557974594ae9772b265907be2151943
andikemgitopsbot <gonzalochief@gmail.com>
2023-12-26 16:45:11 -0500

    add Fast Inverse Sqrt for Float types using generics

--- utils/utils.go
+++ utils/utils.go
@@ -3,7 +3,44 @@ package utils
 import (
 	"errors"
 	"math"
-)
+
+// FastInvSqrt implements Quake III's Fast Inverse Square Root algorythm for Float types (64 and 32)
+// Input:
+// - input: float radicand
+// - iterations: integer with the required estimation iterations (Newton-Raphson approximation)
+// Output:
+// - output: inverse square root
+// - error
+func FastInvSqrt[T constraints.Float](input T, iterations int) (output T, err error) {
+	if input < 0 {
+		return NaN[T](), ErrIndRes
+	}
+	if iterations > 0 {
+		var x2 T = input * 0.5
+		switch any(input).(type) {
+		case float64:
+			i := math.Float64bits(float64(input))
+			i = 0x5FE6EB50C7B537A9 - (i >> 1) // Robertson estimation of the Magic Number
+			output = T(math.Float64frombits(i))
+			output = output * (1.5 - (x2 * output * output))
+			for j := 1; j < iterations; j++ {
+				output = output * (1.5 - (x2 * output * output))
+			}
+		case float32:
+			i := math.Float32bits(float32(input))
+			//i = 0x5F3759DF - (i >> 1) // Original Quake III estimation  of the Magic Number
+			i = 0x5F375A86 - (i >> 1) // Lomont estimation  of the Magic Number
+			output = T(math.Float32frombits(i))
+			output = output * (1.5 - (x2 * output * output))
+			for j := 1; j < iterations; j++ {
+				output = output * (1.5 - (x2 * output * output))
+			}
+		}
+	} else {
+		return NaN[T](), ErrIterMin
+	}
+	return output, nil
+}
 
 // FastInvSqrt32 implements Quake III's Fast Inverse Square Root algorythm for float32 types
 // Input:
