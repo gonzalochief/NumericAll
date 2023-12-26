e629af2347d618aaaf2c3f11fb0c76b2633d4f40
andikemgitopsbot <gonzalochief@gmail.com>
2023-12-26 16:47:44 -0500

    add fast inverse sqrt tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -5,7 +5,53 @@ import (
 	"testing"
 )
 
-// TestInverseSqrt tests the implementation of the fast inverse square root function (Quake III) for float32
+// TestInverseSqrt tests the implementation of the fast inverse square root function (Quake III) for float (32 and 64 bits) (with generics)
+// Assumptions:
+// - Tests estimation against a maximum error of 5 decimal points (epsilon of 1e-5)
+func TestInverseSqrt(t *testing.T) {
+	testCases32 := []struct {
+		input         float32
+		expected      float32
+		iterations    int
+		expectedError error
+	}{
+		{1, 1, 2, nil}, {4, 0.5, 2, nil}, {16, 0.25, 2, nil}, {25, 0.2, 2, nil}, {64, 0.125, 2, nil}, {100, 0.1, 2, nil}, {256, 0.0625, 2, nil}, {400, 0.05, 2, nil}, {625, 0.04, 2, nil}, {1024, 0.03125, 2, nil}, {1024, 0.03125, 0, ErrIterMin}, {-1024, NaN[float32](), 2, ErrIndRes},
+	}
+
+	epsilon32 := float32(1e-5)
+
+	for _, tc := range testCases32 {
+		testValue, err := FastInvSqrt(tc.input, tc.iterations)
+		if err != tc.expectedError {
+			t.Error("iteration is not equal or greater than 1")
+			break
+		} else if Abs(testValue-tc.expected) > epsilon32 {
+			t.Errorf("invalid result. Expecting: %.10f, Receiving: %.10f, Difference: %.10f", tc.expected, testValue, -tc.expected+testValue)
+		}
+	}
+
+	testCases64 := []struct {
+		input         float64
+		expected      float64
+		iterations    int
+		expectedError error
+	}{
+		{1, 1, 2, nil}, {4, 0.5, 2, nil}, {16, 0.25, 2, nil}, {25, 0.2, 2, nil}, {64, 0.125, 2, nil}, {100, 0.1, 2, nil}, {256, 0.0625, 2, nil}, {400, 0.05, 2, nil}, {625, 0.04, 2, nil}, {1024, 0.03125, 2, nil}, {1024, 0.03125, 0, ErrIterMin}, {-1024, NaN[float64](), 2, ErrIndRes},
+	}
+
+	epsilon64 := float64(1e-5)
+
+	for _, tc := range testCases64 {
+		testValue, err := FastInvSqrt(tc.input, tc.iterations)
+		if err != tc.expectedError {
+			t.Error("iteration is not equal or greater than 1")
+			break
+		} else if Abs(testValue-tc.expected) > epsilon64 {
+			t.Errorf("invalid result. Expecting: %.10f, Receiving: %.10f, Difference: %.10f", tc.expected, testValue, -tc.expected+testValue)
+		}
+	}
+}
+
 // Assumptions:
 // - Tests estimation against a maximum error of 5 decimal points (epsilon of 1e-5)
 func TestInverseSqrt32(t *testing.T) {
