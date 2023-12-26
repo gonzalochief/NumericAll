980909bbc7f26d05476a90cc215aca23048d3eec
andikemgitopsbot <gonzalochief@gmail.com>
2023-12-26 16:48:43 -0500

    fix reduced test coverage add fail scenarios for iterations and NaN

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -52,49 +52,54 @@ func TestInverseSqrt(t *testing.T) {
 	}
 }
 
+// TestInverseSqrt32 tests the implementation of the fast inverse square root function (Quake III) for float32
 // Assumptions:
 // - Tests estimation against a maximum error of 5 decimal points (epsilon of 1e-5)
 func TestInverseSqrt32(t *testing.T) {
 	testCases := []struct {
-		input    float32
-		expected float32
+		input         float32
+		expected      float32
+		iterations    int
+		expectedError error
 	}{
-		{1, 1}, {4, 0.5}, {16, 0.25}, {25, 0.2}, {64, 0.125}, {100, 0.1}, {256, 0.0625}, {400, 0.05}, {625, 0.04}, {1024, 0.03125},
+		{1, 1, 2, nil}, {4, 0.5, 2, nil}, {16, 0.25, 2, nil}, {25, 0.2, 2, nil}, {64, 0.125, 2, nil}, {100, 0.1, 2, nil}, {256, 0.0625, 2, nil}, {400, 0.05, 2, nil}, {625, 0.04, 2, nil}, {1024, 0.03125, 2, nil}, {1024, 0.03125, 0, ErrIterMin}, {-1024, NaN[float32](), 2, ErrIndRes},
 	}
 
-	epsilon := float64(1e-5)
+	epsilon := float32(1e-5)
 
 	for _, tc := range testCases {
-		testValue, err := FastInvSqrt32(tc.input, 2)
-		if err != nil {
+		testValue, err := FastInvSqrt32(tc.input, tc.iterations)
+		if err != tc.expectedError {
 			t.Error("iteration is not equal or greater than 1")
 			break
-		} else if math.Abs(float64(testValue)-float64(tc.expected)) > epsilon {
+		} else if Abs(testValue-tc.expected) > epsilon {
 			t.Errorf("invalid result. Expecting: %.10f, Receiving: %.10f, Difference: %.10f", tc.expected, testValue, -tc.expected+testValue)
 		}
 	}
 
 }
 
-// TestInverseSqrt tests the implementation of the fast inverse square root function (Quake III) for float32
+// TestInverseSqrt64 tests the implementation of the fast inverse square root function (Quake III) for float32
 // Assumptions:
 // - Tests estimation against a maximum error of 5 decimal points (epsilon of 1e-5)
 func TestInverseSqrt64(t *testing.T) {
 	testCases := []struct {
-		input    float64
-		expected float64
+		input         float64
+		expected      float64
+		iterations    int
+		expectedError error
 	}{
-		{1, 1}, {4, 0.5}, {16, 0.25}, {25, 0.2}, {64, 0.125}, {100, 0.1}, {256, 0.0625}, {400, 0.05}, {625, 0.04}, {1024, 0.03125},
+		{1, 1, 2, nil}, {4, 0.5, 2, nil}, {16, 0.25, 2, nil}, {25, 0.2, 2, nil}, {64, 0.125, 2, nil}, {100, 0.1, 2, nil}, {256, 0.0625, 2, nil}, {400, 0.05, 2, nil}, {625, 0.04, 2, nil}, {1024, 0.03125, 2, nil}, {1024, 0.03125, 0, ErrIterMin}, {-1024, NaN[float64](), 2, ErrIndRes},
 	}
 
 	epsilon := float64(1e-5)
 
 	for _, tc := range testCases {
-		testValue, err := FastInvSqrt64(tc.input, 2)
-		if err != nil {
+		testValue, err := FastInvSqrt64(tc.input, tc.iterations)
+		if err != tc.expectedError {
 			t.Error("iteration is not equal or greater than 1")
 			break
-		} else if math.Abs(float64(testValue)-float64(tc.expected)) > epsilon {
+		} else if Abs(testValue-tc.expected) > epsilon {
 			t.Errorf("invalid result. Expecting: %.10f, Receiving: %.10f, Difference: %.10f", tc.expected, testValue, -tc.expected+testValue)
 		}
 	}
